c8857a9c8da9501c9cef67224672855950db9cea
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-21 14:03:32 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,14 +3,33 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def new
     @author = Author.new
   end
 
   def create
-    @author = Author.create(author_params)
-
+    @author = Author.new(author_params)
+    if @author.valid?
+        @author.save
+      redirect_to author_path(@author)
+      #binding.pry
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    if @author.save
     redirect_to author_path(@author)
+    else 
+      render :edit
+    end
   end
 
   private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :phone_number, length: {is: 10}, uniqueness: true
+
 end
